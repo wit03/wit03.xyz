3bac96a8e6ba729d3720e2bb802dda1cdb0e75b6
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-18 20:36:09 +0700

    Revert ":sparkles: nav"
    This reverts commit 6d2eebc7b89d50ec8cb4fd6a363bc5349f0348a0.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import App from './_app'
-
-
+import Link from 'next/link'
 
 const Home: React.FC = () => {
+  const [state, setState] = React.useState(0)
   return (
     <main>
-      <App />
+      <h1>Hello from Preact</h1>
+      <p>{state}</p>
+      <button onClick={() => setState(state + 1)}>Increment</button>
     </main>
   )
 }
