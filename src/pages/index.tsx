def17d561bde83c5d60817f893127b68e697df01
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-18 17:49:41 +0700

    :zap: Init tailwind.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
+import App from './app'
 
 const Home: React.FC = () => {
-  const [state, setState] = React.useState(0)
   return (
-    <main>
-      <h1>Hello from Preact</h1>
-      <p>{state}</p>
-      <button onClick={() => setState(state + 1)}>Increment</button>
-    </main>
+    <React.Fragment> 
+      <App/>
+    </React.Fragment>
   )
 }
 
