83ac1f9e5ceead1cab6c1de3b5c11892d6113b27
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-23 12:22:59 +0700

    :hammer: map projects data

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const Projects: React.FC = () => {
+import { projects } from '../../store/projects';
+
+const Projects:React.FC = () => {
   return (
     <section className='text-gray-700 body-font'>
       <div className='container px-5 py-24 mx-auto'>
@@ -15,91 +17,24 @@ const Projects: React.FC = () => {
           </p>
         </div>
         <div className='flex flex-wrap -m-4'>
-          <div className='xl:w-1/3 md:w-1/2 p-4'>
+          {
+            projects.map((data) => (
+              <div className='xl:w-1/3 md:w-1/2 p-4'>
             <div className='border border-gray-300 p-6 rounded-lg'>
               <span className='inline-block bg-indigo-100 rounded-full px-3 py-1 text-sm font-normal text-indigo-500 mr-2 mb-3'>
-                #photography
+                {data.tag}
               </span>
 
               <h2 className='text-lg text-gray-900 font-medium title-font mb-2'>
-                Shooting Stars
+                {data.name}
               </h2>
               <p className='leading-relaxed text-base'>
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
+                {data.description}
               </p>
             </div>
           </div>
-          <div className='xl:w-1/3 md:w-1/2 p-4'>
-            <div className='border border-gray-300 p-6 rounded-lg'>
-              <span className='inline-block bg-indigo-100 rounded-full px-3 py-1 text-sm font-normal text-indigo-500 mr-2 mb-3'>
-                #photography
-              </span>
-              <h2 className='text-lg text-gray-900 font-medium title-font mb-2'>
-                The Catalyzer
-              </h2>
-              <p className='leading-relaxed text-base'>
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
-          <div className='xl:w-1/3 md:w-1/2 p-4'>
-            <div className='border border-gray-300 p-6 rounded-lg'>
-              <span className='inline-block bg-indigo-100 rounded-full px-3 py-1 text-sm font-normal text-indigo-500 mr-2 mb-3'>
-                #photography
-              </span>
-              <h2 className='text-lg text-gray-900 font-medium title-font mb-2'>
-                Neptune
-              </h2>
-              <p className='leading-relaxed text-base'>
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
-          <div className='xl:w-1/3 md:w-1/2 p-4'>
-            <div className='border border-gray-300 p-6 rounded-lg'>
-              <span className='inline-block bg-indigo-100 rounded-full px-3 py-1 text-sm font-normal text-indigo-500 mr-2 mb-3'>
-                #photography
-              </span>
-              <h2 className='text-lg text-gray-900 font-medium title-font mb-2'>
-                Melanchole
-              </h2>
-              <p className='leading-relaxed text-base'>
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
-          <div className='xl:w-1/3 md:w-1/2 p-4'>
-            <div className='border border-gray-300 p-6 rounded-lg'>
-              <span className='inline-block bg-indigo-100 rounded-full px-3 py-1 text-sm font-normal text-indigo-500 mr-2 mb-3'>
-                #photography
-              </span>
-              <h2 className='text-lg text-gray-900 font-medium title-font mb-2'>
-                Bunker
-              </h2>
-              <p className='leading-relaxed text-base'>
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
-          <div className='xl:w-1/3 md:w-1/2 p-4'>
-            <div className='border border-gray-300 p-6 rounded-lg'>
-              <span className='inline-block bg-indigo-100 rounded-full px-3 py-1 text-sm font-normal text-indigo-500 mr-2 mb-3'>
-                #photography
-              </span>
-              <h2 className='text-lg text-gray-900 font-medium title-font mb-2'>
-                Ramona Falls
-              </h2>
-              <p className='leading-relaxed text-base'>
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
+            ))
+          }
         </div>
         <button className='flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
           Button
--- src/store/projects.ts
+++ src/store/projects.ts
@@ -1,5 +1,3 @@
-import { link } from "fs";
-
 interface Projects {
     name: String,
     tag: String,
@@ -10,37 +8,37 @@ interface Projects {
 export const projects: Projects[] = [
     {
         name: "Relationship Detector",
-        tag: "web",
+        tag: "#web",
         description: "Using machine learning to classify your relation status by picture.",
         link: "https://github.com/wit03/RelationshipDetector"
     },
     {
         name: "Dusit Thani Blog",
-        tag: "web",
+        tag: "#web",
         description: "A blog for Dusit Thani Hotel located in Thailand.",
         link: "dusitthaniwelove.com"
     },
     {
         name: "Optimized Plotter CNC",
-        tag: "electronics",
+        tag: "#electronics",
         description: "I built a plotter cnc by combined an old CD tray with the magic of Arduino.",
         link: "https://github.com/wit03/optimized-plotter-cnc"
     },
     {
         name: "RA Club Registration System",
-        tag: "web",
+        tag: "#web",
         description: "A web-based registration system for my school using React.js and MongoDB.",
         link: "https://github.com/roong-aroon-developer/RA-club-registeration"
     },
     {
         name: "Traffic Jamn't",
-        tag: "hackathon",
+        tag: "#hackathon",
         description: "The best way to avoid traffic jam is to run around.",
         link: "https://github.com/EMWPevilCo/traffic-jamnt"
     },
     {
         name: "RA PM2.5",
-        tag: "API",
+        tag: "#api",
         description: "PM2.5 Rest API for my school using Express.js and Node.js",
         link: "https://github.com/roong-aroon-developer/Roong-Aroon-PM25-API"
     }
