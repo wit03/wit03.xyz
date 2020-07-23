730da79648ba6e47a3c287de6adbf003480b812d
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-24 00:45:10 +0700

    :hammer: add education section

---
+++ src/components/main/educations.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { educations } from '../../store/educations'
+
+const Educations: React.FC = () => {
+  return (
+    <section className='text-gray-700 body-font overflow-hidden'>
+      <div className='container px-5 pt-16 pb-24 mx-auto'>
+      <h1 className='sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 pb-2'>
+              Educations
+            </h1>
+        <div className='-my-8'>
+        {
+        educations.map((data) => (
+            <div className='py-8 flex flex-wrap md:flex-no-wrap'>
+            <div className='md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col'>
+              <span className='tracking-widest font-medium title-font text-gray-900'>
+                {data.catagory}
+              </span>
+              <span className='mt-1 text-gray-500 text-sm'>{data.year}</span>
+            </div>
+            <div className='md:flex-grow'>
+              <h2 className='text-2xl font-medium text-gray-900 title-font mb-2'>
+                {data.name}
+              </h2>
+              <p className='leading-relaxed'>
+                {data.description}
+              </p>
+              <a className='text-indigo-500 inline-flex items-center mt-4'>
+                Learn More
+                <svg
+                  className='w-4 h-4 ml-2'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  stroke-width='2'
+                  fill='none'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'>
+                  <path d='M5 12h14'></path>
+                  <path d='M12 5l7 7-7 7'></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        ))
+    }
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Educations
---
+++ src/store/educations.ts
@@ -0,0 +1,24 @@
+interface educationTypes {
+    catagory: String,
+    name: String,
+    year: String,
+    description: String,
+    link: String 
+}
+
+export const educations:educationTypes[] = [
+    {
+        catagory: "High School",
+        name: "Roong Aroon School",
+        year: "2006-Now",
+        description: "Roong Aroon School is an alternative school that teaching students by project-based learning so we learn lots of thing from project research",
+        link: "http://roong-aroon.ac.th"
+    },
+    {
+        catagory: "Summmer Course",
+        name: "Kwalicum Secondary School",
+        year: "2016",
+        description: "Roong Aroon School is an alternative school that teaching students by project-based learning so we learn lots of thing from project research",
+        link: "http://roong-aroon.ac.th"
+    }
+]
