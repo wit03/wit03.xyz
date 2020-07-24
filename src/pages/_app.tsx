08d754f431a02c6c67e866568da063ea9165438b
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-24 14:14:36 +0700

    :hammer: add path alias 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 import 'react-multi-carousel/lib/styles.css'
 import '../styles/tailwind.css'
 
-import Education from '../components/main/educations';
-import Hobbies from '../components/main/activities'
-import Profile from '../components/main/profile'
-import Projects from '../components/main/projects'
-import Skills from '../components/main/skills'
-import Educations from '../components/main/educations';
+import Hobbies from 'components/main/activities'
+import Profile from 'components/main/profile'
+import Projects from 'components/main/projects'
+import Skills from 'components/main/skills'
+import Educations from 'components/main/educations';
 
 
 const App: React.FC = () => {
