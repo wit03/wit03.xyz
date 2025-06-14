ccc547fe4c964911fd55e3caa598a9572ac74468
wit03 <witjarukit@gmail.com>
2025-06-15 01:34:23 +0700

    feat(homepage): Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        scaleAnim: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.1)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        shine: {
+          '0%': { 'background-position': '0% 50%' },
+          '100%': { 'background-position': '100% 50%' },
+        },
+      },
+      animation: {
+        scale: 'scaleAnim 300ms ease-in-out',
+        scroll: 'scroll 60s linear infinite',
+        'scroll-md': 'scroll 50s linear infinite',
+        paused: 'none',
+        shine: 'shine 3s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
